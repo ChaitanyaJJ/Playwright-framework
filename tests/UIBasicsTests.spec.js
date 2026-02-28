a9f5f294dc5861b97e9633209bcaaf8562d35d2d
Chaitanya <chaitanya@mac.home>
2026-02-28 14:55:54 +0000

    Getting started with Playwright Automation Core concepts

---
+++ tests/UIBasicsTests.spec.js
@@ -0,0 +1,18 @@
+const {test,expect} = require('@playwright/test');
+
+
+test('First Playwright test', async ({browser})=>
+{
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+});
+
+//shorter way to declare
+test.only('Page Playwright test', async ({page})=>
+{
+    await page.goto("https://google.com");
+    console.log(await page.title());
+    await expect(page).toHaveTitle("Google");
+});
