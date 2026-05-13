6697d646670a685dbd692a7d06d12f8e494fe572
Chaitanya <jchaitanya16@outlook.com>
2026-05-13 10:56:27 +0100

    Extracting text and assertions

--- tests/UIBasicsTests.spec.js
+++ tests/UIBasicsTests.spec.js
@@ -3,22 +3,30 @@ const {test,expect} = require('@playwright/test');
 
 test.only('Browser Context Playwright test', async ({browser})=>
 {
+   
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
-    await page.locator('#username').fill('rahulshetty');
-    await page.locator("[type='password']").fill('learning');
-    await page.locator('#signInBtn').click();
+    await userName.fill('rahulshetty');
+    await page.locator("[type='password']").fill('Learning@830$3mK2');
+    await signIn.click();
     console.log(await page.locator("[style*='block']").textContent());
+    await expect(await page.locator("[style*='block']")).toContainText('Incorrect');
 
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy")
+    await signIn.click();
+    console.log(await page.locator('.card-title a').first().textContent());
 
 });
 
 //shorter way to declare
-test.only('Page Playwright test', async ({page})=>
-{
-    await page.goto("https://google.com");
-    console.log(await page.title());
-    await expect(page).toHaveTitle("Google");
-});
+// test.only('Page Playwright test', async ({page})=>
+// {
+//     await page.goto("https://google.com");
+//     console.log(await page.title());
+//     await expect(page).toHaveTitle("Google");
+// });
