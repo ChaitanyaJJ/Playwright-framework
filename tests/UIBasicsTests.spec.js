ba0aaa9a1e0fe72dce9a0855269349e74a3b32ef
Chaitanya <jchaitanya16@outlook.com>
2026-07-04 12:05:06 +0100

    restart

--- tests/UIBasicsTests.spec.js
+++ tests/UIBasicsTests.spec.js
@@ -8,6 +8,7 @@ test.only('Browser Context Playwright test', async ({browser})=>
     const page = await context.newPage();
     const userName = page.locator('#username');
     const signIn = page.locator('#signInBtn');
+    const cardTitles = page.locator('.card-title a');
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
     await userName.fill('rahulshetty');
@@ -19,7 +20,10 @@ test.only('Browser Context Playwright test', async ({browser})=>
     await userName.fill("");
     await userName.fill("rahulshettyacademy")
     await signIn.click();
-    console.log(await page.locator('.card-title a').first().textContent());
+    console.log(await cardTitles.first().textContent());
+    const allTitles = await cardTitles.allTextContents();
+    console.log(allTitles)
+
 
 });
 
